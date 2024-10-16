922557c0a55f96aa5d6cc5d815add0eaeb4a546c
macovei-alexandru <maccelmare@gmail.com>
2024-10-17 00:00:32 +0300

    properly configured nativewind

--- frontend/QClient/app/index.jsx
+++ frontend/QClient/app/index.jsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Hello world!</Text>
+    <View className="flex-1 items-center justify-center bg-bg-100">
+      <Text className="text-3xl text-txt-100">Hello world!</Text>
     </View>
   );
 }
--- frontend/QClient/tailwind.config.js
+++ frontend/QClient/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: {
+          100: "#ffffff",
+          200: "#c9d3c4",
+          300: "#5ab02d",
+          400: "#cf4949",
+        },
+        txt: {
+          100: "#000000",
+          200: "#898080",
+          300: "#ffffff",
+        },
+      },
+    },
   },
   plugins: [],
 };
