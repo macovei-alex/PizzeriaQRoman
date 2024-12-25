903885cb0d2538b86fa53089a95a30428e564c00
macovei-alexandru <maccelmare@gmail.com>
2024-12-25 16:12:15 +0200

    added test configuration profile

--- backend/qservices/src/main/java/ro/pizzeriaq/qservices/QservicesApplication.java
+++ backend/qservices/src/main/java/ro/pizzeriaq/qservices/QservicesApplication.java
@@ -1,9 +1,13 @@
 package ro.pizzeriaq.qservices;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ro.pizzeriaq.qservices.service.EntityInitializerService;
@@ -11,6 +15,13 @@ import ro.pizzeriaq.qservices.service.EntityInitializerService;
 
 @SpringBootApplication
 public class QservicesApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(QservicesApplication.class);
+
+
+	@Value("${application.environment}")
+	private String environment;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(QservicesApplication.class, args);
@@ -30,7 +41,12 @@ public class QservicesApplication {
 		};
 	}
 
+	@Bean
+	public CommandLineRunner logEnvironment() {
+		return (_) -> logger.info("Environment: {}", environment);
+	}
 
+	@Profile("default")
 	@Bean
 	public CommandLineRunner initializeEntities(EntityInitializerService entityInitializerService) {
 		return (_) -> {
--- backend/qservices/src/test/java/ro/pizzeriaq/qservices/controllers/ProductControllerTest.java
+++ backend/qservices/src/test/java/ro/pizzeriaq/qservices/controllers/ProductControllerTest.java
@@ -1,9 +1,15 @@
 package ro.pizzeriaq.qservices.controllers;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import ro.pizzeriaq.qservices.controller.ProductController;
 import ro.pizzeriaq.qservices.service.ProductService;
@@ -16,13 +22,28 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ProductController.class)
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ProductControllerTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(ProductControllerTest.class);
+
+
+	@Value("${application.environment}")
+	private String environment;
+
 
 	@Autowired
 	private MockMvc mockMvc;
 
 	@MockBean
 	private ProductService productService;
+
+
+	@BeforeAll
+	void beforeAll() {
+		logger.info("Environment: {}", environment);
+	}
 
 
 	@Test
