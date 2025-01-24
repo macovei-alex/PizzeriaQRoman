fb3cd3e9ac9d84830997154a4d30fec5e05ec89a
macovei-alexandru <maccelmare@gmail.com>
2025-01-24 19:54:58 +0200

    adjusted .eslint.js

--- frontend/QClient/.eslintrc.js
+++ frontend/QClient/.eslintrc.js
@@ -1,5 +1,24 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
-  ignorePatterns: ['/dist/*'],
+  env: {
+    es2021: true, // Enables ES2021 syntax
+  },
+  extends: ["expo"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true, // Enable linting for JSX
+    },
+    ecmaVersion: 12, // Use modern JavaScript syntax
+    sourceType: "module", // Support ES modules
+  },
+  plugins: ["react"], // Include the React plugin
+  rules: {
+    "react/prop-types": "off", // Disable prop-types validation (optional)
+  },
+  settings: {
+    react: {
+      version: "detect", // Automatically detect React version
+    },
+  },
+  ignorePatterns: ["/dist/*"],
 };
--- frontend/QClient/components/menu/product/HorizontalLine.jsx
+++ frontend/QClient/components/menu/product/HorizontalLine.jsx
@@ -1,4 +1,5 @@
 import { StyleSheet, View } from "react-native";
+import React from "react";
 
 export default function HorizontalLine({ style }) {
   return <View style={[styles.line, style]}></View>;
