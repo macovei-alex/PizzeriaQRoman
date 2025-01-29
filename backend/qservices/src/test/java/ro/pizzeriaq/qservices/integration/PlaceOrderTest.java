d02495c52bc4203b12beb48fb36275fa8d7c1bbc
macovei-alexandru <maccelmare@gmail.com>
2025-01-29 18:39:32 +0200

    added PlaceOrderTest class

--- backend/qservices/src/test/java/ro/pizzeriaq/qservices/integration/PlaceOrderTest.java
+++ backend/qservices/src/test/java/ro/pizzeriaq/qservices/integration/PlaceOrderTest.java
@@ -22,21 +22,21 @@ import java.util.Comparator;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureMockMvc
-class ProductTests {
+public class PlaceOrderTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(PlaceOrderTest.class);
 
 	@Value("${server.servlet.context-path}")
 	private String contextPath;
 
 	@Value("${application.environment}")
 	private String environment;
-
-
-	private static final Logger logger = LoggerFactory.getLogger(ProductTests.class);
 
 
 	@Autowired
@@ -56,6 +56,8 @@ class ProductTests {
 		entityInitializerService.deleteAll();
 		entityInitializerService.addProducts();
 		entityInitializerService.addOptionLists();
+		entityInitializerService.addAccounts();
+		entityInitializerService.addOrders();
 		entityInitializerService.bindOptionsToProducts();
 	}
 
@@ -73,56 +75,5 @@ class ProductTests {
 	@Test
 	void entitiesInitializationTest() {
 		assertThat(productService.getProducts()).isNotEmpty();
-	}
-
-	@Test
-	void testGetAllProducts() throws Exception {
-		mockMvc.perform(get(contextPath + "/product/all")
-						.contextPath(contextPath)
-						.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$").isArray())
-				.andExpect(jsonPath("$.length()").value(8));
-	}
-
-	@Test
-	void testGetNonexistentProduct() throws Exception {
-		mockMvc.perform(get(contextPath + "/product/{id}", Integer.MAX_VALUE)
-						.contextPath(contextPath)
-						.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isNoContent());
-	}
-
-	@Test
-	void testGetProductWithWrongRoute() throws Exception {
-		mockMvc.perform(get(contextPath + "/product/invalid")
-						.contextPath(contextPath)
-						.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isBadRequest());
-	}
-
-	@Test
-	void getProductWithValidId() throws Exception {
-		var product = productService.getProduct(
-						productService.getProducts()
-								.stream()
-								.sorted(Comparator.comparing(ProductDTO::getName))
-								.toList()
-								.get(2)
-								.getId())
-				.orElse(null);
-
-		assertThat(product).isNotNull();
-
-		mockMvc.perform(get(contextPath + "/product/{id}", product.getId())
-						.contextPath(contextPath)
-						.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-				.andExpect(jsonPath("$.id").value(product.getId()))
-				.andExpect(jsonPath("$.name").value("Pizza Capriciosa"))
-				.andExpect(jsonPath("$.optionLists").isArray())
-				.andExpect(jsonPath("$.optionLists.length()").value(3));
 	}
 }
---
+++ backend/qservices/src/test/java/ro/pizzeriaq/qservices/integration/ProductTest.java
@@ -0,0 +1,128 @@
+package ro.pizzeriaq.qservices.integration;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import ro.pizzeriaq.qservices.service.DTO.ProductDTO;
+import ro.pizzeriaq.qservices.service.EntityInitializerService;
+import ro.pizzeriaq.qservices.service.ProductService;
+
+import java.util.Comparator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureMockMvc
+class ProductTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(ProductTest.class);
+
+
+	@Value("${server.servlet.context-path}")
+	private String contextPath;
+
+	@Value("${application.environment}")
+	private String environment;
+
+
+	@Autowired
+	private EntityInitializerService entityInitializerService;
+
+	@Autowired
+	private ProductService productService;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+
+	@BeforeAll
+	void setUp() {
+		logger.info("Environment: {}", environment);
+
+		entityInitializerService.deleteAll();
+		entityInitializerService.addProducts();
+		entityInitializerService.addOptionLists();
+		entityInitializerService.bindOptionsToProducts();
+	}
+
+	@AfterAll
+	void tearDown() {
+		entityInitializerService.deleteAll();
+	}
+
+
+	@Test
+	void contextLoads() {
+		assertThat(mockMvc).isNotNull();
+	}
+
+	@Test
+	void entitiesInitializationTest() {
+		assertThat(productService.getProducts()).isNotEmpty();
+	}
+
+	@Test
+	void testGetAllProducts() throws Exception {
+		mockMvc.perform(get(contextPath + "/product/all")
+						.contextPath(contextPath)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$").isArray())
+				.andExpect(jsonPath("$.length()").value(8));
+	}
+
+	@Test
+	void testGetNonexistentProduct() throws Exception {
+		mockMvc.perform(get(contextPath + "/product/{id}", Integer.MAX_VALUE)
+						.contextPath(contextPath)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNoContent());
+	}
+
+	@Test
+	void testGetProductWithWrongRoute() throws Exception {
+		mockMvc.perform(get(contextPath + "/product/invalid")
+						.contextPath(contextPath)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	void getProductWithValidId() throws Exception {
+		var product = productService.getProduct(
+						productService.getProducts()
+								.stream()
+								.sorted(Comparator.comparing(ProductDTO::getName))
+								.toList()
+								.get(2)
+								.getId())
+				.orElse(null);
+
+		assertThat(product).isNotNull();
+
+		mockMvc.perform(get(contextPath + "/product/{id}", product.getId())
+						.contextPath(contextPath)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.id").value(product.getId()))
+				.andExpect(jsonPath("$.name").value("Pizza Capriciosa"))
+				.andExpect(jsonPath("$.optionLists").isArray())
+				.andExpect(jsonPath("$.optionLists.length()").value(3));
+	}
+}
