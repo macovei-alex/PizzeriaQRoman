762b54379a3833ff3fa0b629adfa61f2d3016198
macovei-alexandru <maccelmare@gmail.com>
2025-01-29 17:56:48 +0200

    updated validation error message

--- backend/qservices/src/main/java/ro/pizzeriaq/qservices/service/DTO/PlacedOrderItemDTO.java
+++ backend/qservices/src/main/java/ro/pizzeriaq/qservices/service/DTO/PlacedOrderItemDTO.java
@@ -11,10 +11,10 @@ import lombok.*;
 @Builder
 public class PlacedOrderItemDTO {
 
-	@Min(value = 1, message = "You cannot order a product with the ID equal to 0")
+	@Min(value = 1, message = "You cannot order a product with the ID less than or equal to 0")
 	private int productId;
 
-	@Min(value = 1, message = "You cannot order cannot contain 0 or less items of a kind")
+	@Min(value = 1, message = "You cannot order an amount of items less than or equal to 0")
 	private int count;
 
 }
