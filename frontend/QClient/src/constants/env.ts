37724c1acbe8fd9991fc5e27076421fbb3443a91
macovei-alexandru <maccelmare@gmail.com>
2025-04-25 22:41:54 +0300

    fixed proces.env typing potential bundler problems

--- frontend/QClient/src/constants/env.ts
+++ frontend/QClient/src/constants/env.ts
@@ -5,4 +5,9 @@ const envValidation = z.object({
   EXPO_PUBLIC_KEYCLOAK_CLIENT_ID: z.string(),
 });
 
-export const ENV = envValidation.parse(process.env);
+const env = process.env as any;
+
+export const ENV = envValidation.parse({
+  EXPO_PUBLIC_KEYCLOAK_REALM_URL: env.EXPO_PUBLIC_KEYCLOAK_REALM_URL,
+  EXPO_PUBLIC_KEYCLOAK_CLIENT_ID: env.EXPO_PUBLIC_KEYCLOAK_CLIENT_ID,
+});
