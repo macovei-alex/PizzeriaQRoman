900fe7558c45412c81dcfb22e32338a5f3f3bc1a
macovei-alexandru <maccelmare@gmail.com>
2025-05-02 00:33:51 +0300

    improved cart item card design

--- frontend/QClient/src/components/cart/CartScreen/CartItemCard.tsx
+++ frontend/QClient/src/components/cart/CartScreen/CartItemCard.tsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { imageOrDefault } from "src/utils/files";
 import useSingleImage from "src/hooks/useSingleImage";
 import useColorTheme from "src/hooks/useColorTheme";
-import HorizontalLine from "src/components/shared/ProductScreen/HorizontalLine";
 import PlusCircleSvg from "src/components/svg/PlusCircleSvg";
 import MinusCircleSvg from "src/components/svg/MinusCircleSvg";
 import { CartItem, useCartContext } from "src/context/CartContext";
@@ -27,112 +26,133 @@ export default function CartItemCard({ cartItem, price }: CartItemCardProps) {
   const colorTheme = useColorTheme();
   const { changeCartItemCount } = useCartContext();
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.infoSectionContainer}>
-        <TouchableOpacity
-          style={styles.imageContainer}
-          onPress={() =>
-            navigation.navigate("ProductScreen", {
-              productId: cartItem.product.id.toString(),
-              imageName: cartItem.product.imageName,
-              cartItemId: cartItem.id.toString(),
+  const selectedOptionsStr = useMemo(
+    () =>
+      Object.entries(cartItem.options)
+        .map(([optionListId, selectedOptions]) =>
+          Object.entries(selectedOptions)
+            .map(([optionId, count]) => {
+              const optionList = cartItem.product.optionLists.find(
+                (list) => list.id === Number(optionListId)
+              );
+              if (!optionList) throw new Error(`Option list not found: ${optionListId}`);
+              const option = optionList.options.find((option) => option.id === Number(optionId));
+              if (!option) throw new Error(`Option not found: ${optionId}`);
+              if (optionList.options[0].maxCount > 1) return `${count} x ${option.name}`;
+              return option.name;
             })
-          }
+            .join(", ")
+        )
+        .join(", "),
+    [cartItem.options, cartItem.product.optionLists]
+  );
+
+  return (
+    <View style={[styles.container, { backgroundColor: colorTheme.background.card }]}>
+      {/* image */}
+      <TouchableOpacity
+        style={styles.imageContainer}
+        onPress={() =>
+          navigation.navigate("ProductScreen", {
+            productId: cartItem.product.id.toString(),
+            imageName: cartItem.product.imageName,
+            cartItemId: cartItem.id.toString(),
+          })
+        }
+      >
+        <Image source={imageOrDefault(image)} style={styles.image} />
+      </TouchableOpacity>
+
+      {/* info section */}
+      <View style={styles.infoSectionContainer}>
+        {/* product title */}
+        <Text
+          style={[styles.productNameText, { color: colorTheme.text.primary }]}
+          numberOfLines={1}
+          ellipsizeMode="tail"
         >
-          <Image source={imageOrDefault(image)} style={styles.image} />
-        </TouchableOpacity>
-        <View style={styles.textSectionContainer}>
-          <Text
-            style={[styles.productNameText, { color: colorTheme.text.primary }]}
-            numberOfLines={1}
-            ellipsizeMode="tail"
-          >
-            {cartItem.product.name}
-          </Text>
-          <Text
-            style={[styles.productSubtitleText, { color: colorTheme.text.primary }]}
-            numberOfLines={1}
-            ellipsizeMode="tail"
-          >
-            {cartItem.product.subtitle}
-          </Text>
-          <View style={styles.productDescriptionContainer}>
-            <Text
-              style={[styles.productDescriptionText, { color: colorTheme.text.secondary }]}
-              numberOfLines={3}
-              ellipsizeMode="tail"
-            >
-              {cartItem.product.description}
-            </Text>
+          {cartItem.product.name}
+        </Text>
+
+        {/* product subtitle */}
+        <Text
+          style={[styles.productSubtitleText, { color: colorTheme.text.primary }]}
+          numberOfLines={1}
+          ellipsizeMode="tail"
+        >
+          {cartItem.product.subtitle}
+        </Text>
+
+        {/* product options */}
+        <Text style={[styles.productOptionsText, { color: colorTheme.text.secondary }]}>
+          {selectedOptionsStr}
+        </Text>
+
+        {/* price section */}
+        <View style={styles.priceSectionContainer}>
+          <View style={[styles.priceContainer, { backgroundColor: colorTheme.background.accent }]}>
+            <Text style={[styles.priceText, { color: colorTheme.text.onAccent }]}>{formatPrice(price)}</Text>
           </View>
+
+          {/* minus */}
+          <TouchableOpacity onPress={() => changeCartItemCount(cartItem.id, -1)}>
+            <MinusCircleSvg style={styles.plusMinusSvg} />
+          </TouchableOpacity>
+
+          {/* count */}
+          <Text style={styles.itemCountText}>{cartItem.count}</Text>
+
+          {/* plus */}
+          <TouchableOpacity onPress={() => changeCartItemCount(cartItem.id, 1)}>
+            <PlusCircleSvg style={styles.plusMinusSvg} />
+          </TouchableOpacity>
         </View>
       </View>
-
-      <View style={styles.priceSectionContainer}>
-        <View style={[styles.priceContainer, { backgroundColor: colorTheme.background.accent }]}>
-          <Text style={[styles.priceText, { color: colorTheme.text.onAccent }]}>{formatPrice(price)}</Text>
-        </View>
-        <TouchableOpacity onPress={() => changeCartItemCount(cartItem.id, -1)}>
-          <MinusCircleSvg style={styles.plusMinusSvg} />
-        </TouchableOpacity>
-        <Text style={styles.itemCountText}>{cartItem.count}</Text>
-        <TouchableOpacity onPress={() => changeCartItemCount(cartItem.id, 1)}>
-          <PlusCircleSvg style={styles.plusMinusSvg} />
-        </TouchableOpacity>
-      </View>
-
-      <HorizontalLine style={[styles.hr, { backgroundColor: colorTheme.background.card }]} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginHorizontal: 12,
-    marginVertical: 12,
-  },
-  infoSectionContainer: {
     flexDirection: "row",
     gap: "5%",
+    margin: 8,
+    padding: 12,
+    borderRadius: 12,
   },
   imageContainer: {
-    width: "35%",
+    width: "25%",
   },
   image: {
     aspectRatio: 1,
     borderRadius: 9999,
   },
-  textSectionContainer: {
-    maxWidth: "60%",
+  infoSectionContainer: {
+    width: "70%",
   },
   productNameText: {
     fontSize: 20,
     fontWeight: "bold",
   },
   productSubtitleText: {
-    fontSize: 18,
+    fontSize: 16,
   },
-  productDescriptionContainer: {
+  productOptionsText: {
     marginTop: 4,
-  },
-  productDescriptionText: {
     fontSize: 12,
     fontWeight: "light",
-  },
-  hr: {
-    marginTop: 24,
   },
   priceSectionContainer: {
     flexDirection: "row",
     justifyContent: "flex-end",
     alignItems: "center",
+    marginTop: 16,
   },
   priceContainer: {
+    marginRight: 12,
     paddingVertical: 8,
     paddingHorizontal: 12,
     borderRadius: 8,
-    marginRight: 20,
   },
   priceText: {
     fontSize: 18,
@@ -142,7 +162,7 @@ const styles = StyleSheet.create({
     height: 35,
   },
   itemCountText: {
-    marginHorizontal: 4,
+    marginHorizontal: 6,
     fontSize: 20,
     fontWeight: "bold",
   },
