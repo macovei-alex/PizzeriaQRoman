4d292073ad2220d913df593b55f3d7230d0e755e
macovei-alexandru <maccelmare@gmail.com>
2025-05-11 11:48:53 +0300

    improved error component

--- frontend/QClient/src/components/shared/generic/ErrorComponent.tsx
+++ frontend/QClient/src/components/shared/generic/ErrorComponent.tsx
@@ -1,24 +1,92 @@
 import { useQueryClient } from "@tanstack/react-query";
+import { Image } from "expo-image";
 import React from "react";
-import { Button, Text } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "src/constants";
+import { useAuthContext } from "src/context/AuthContext";
+import useColorTheme from "src/hooks/useColorTheme";
 
 type ErrorComponentProps = {
   onRetry?: () => void;
 };
 
 export default function ErrorComponent({ onRetry }: ErrorComponentProps) {
+  const authContext = useAuthContext();
   const queryClient = useQueryClient();
+  const colorTheme = useColorTheme();
+
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Something went wrong</Text>
-      <Button
-        title="Retry"
-        onPress={() => {
-          queryClient.invalidateQueries({ refetchType: "active" });
-          if (onRetry) onRetry();
-        }}
-      />
+    <SafeAreaView style={[styles.container, { backgroundColor: colorTheme.background.primary }]}>
+      {/* title */}
+      <Text style={[styles.title, { color: colorTheme.text.primary }]}>Ceva nu a mers bine...</Text>
+
+      {/* image */}
+      <Image source={images.sadChef} style={styles.image} />
+
+      <View style={styles.buttonSection}>
+        {/* retry button */}
+        <TouchableOpacity
+          style={[styles.buttonContainer, { backgroundColor: colorTheme.background.accent }]}
+          onPress={() => {
+            queryClient.invalidateQueries({ refetchType: "active" });
+            if (onRetry) onRetry();
+          }}
+        >
+          <Text style={[styles.buttonText, { color: colorTheme.text.onAccent }]}>Reîncercare</Text>
+        </TouchableOpacity>
+
+        {/* disconnect button */}
+        <TouchableOpacity
+          style={[
+            styles.buttonContainer,
+            styles.buttonBorder,
+            { backgroundColor: colorTheme.background.primary, borderColor: colorTheme.text.primary },
+          ]}
+          onPress={() => {
+            queryClient.invalidateQueries({ refetchType: "none" });
+            authContext.logout();
+          }}
+        >
+          <Text style={[styles.buttonText, { color: colorTheme.text.primary }]}>Deconectare</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 32,
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  buttonSection: {
+    width: "100%",
+    gap: 16,
+  },
+  buttonContainer: {
+    padding: 12,
+    borderRadius: 12,
+    width: "100%",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "400",
+  },
+  buttonBorder: {
+    borderWidth: 1,
+  },
+});
--- frontend/QClient/src/constants/images.ts
+++ frontend/QClient/src/constants/images.ts
@@ -1,5 +1,6 @@
 import defaultPizza from "src/../assets/images/default-pizza.jpg";
 import menuBackground from "src/../assets/images/menu-background.png";
 import logo from "src/../assets/images/logo.png";
+import sadChef from "src/../assets/images/sad-chef.png";
 
-export const images = { defaultPizza, menuBackground, logo };
+export const images = { defaultPizza, menuBackground, logo, sadChef };
